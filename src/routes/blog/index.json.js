aee09ba438bfa71ab68b34eec2951df6c029deea
jasonmelis <jasonmelis@penguin.lxd>
2020-07-15 16:26:21 -0400

    Initial commit

---
+++ src/routes/blog/index.json.js
@@ -0,0 +1,42 @@
+import fs from 'fs';
+import path from 'path';
+import grayMatter from 'gray-matter';
+
+const getAllPosts = () => {
+  try {
+    return fs.readdirSync('static/posts').map((filename) => {
+      const post = fs.readFileSync(
+        path.resolve('static/posts', filename),
+        'utf-8'
+      );
+      return grayMatter(post).data;
+    });
+  } catch (e) {
+    return [];
+  }
+};
+
+export function get(_, res) {
+  res.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+  const posts = getAllPosts();
+  res.end(JSON.stringify(posts));
+}
+
+// import posts from './_posts.js';
+
+// const contents = JSON.stringify(posts.map(post => {
+// 	return {
+// 		title: post.title,
+// 		slug: post.slug
+// 	};
+// }));
+
+// export function get(req, res) {
+// 	res.writeHead(200, {
+// 		'Content-Type': 'application/json'
+// 	});
+
+// 	res.end(contents);
+// }
