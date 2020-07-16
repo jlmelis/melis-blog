4fec3651f7bf083dddbb76a7c84231e4f583ff83
jasonmelis <jasonmelis@penguin.lxd>
2020-07-16 10:56:16 -0400

    Starting to customize

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,36 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<figure>
+	<img alt='Success Kid' src='villageBlacksmith.jpg'>
+</figure>
 
-<p>This is the 'about' page. There's not much here.</p>
+<h1>About this site</h1>
+<p>Hi! My name is Jason Melis, and this is my site! I am using this site  
+  as a home base for learning and showcasing new technology and as a way to put some of my 
+  thoughts into the world. The content of this site will be comprised of information from two of 
+  my passions: Programming and Blacksmithing.</p>
+  
+  
+<p>This site has been made using the following: </p>
+<ul>
+  <li><a href="https://sapper.svelte.dev/" target="_blank">Sapper</a></li>
+  <li><a href="https://www.netlifycms.org/" target="_blank">Netlify CMS</a></li>
+</ul>
+
+<p>The code for this site and all of my little projects can be found on 
+  <a href="https://github.com/jlmelis" target="_blank">Github</a></p>
+
+<style>
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	} 
+
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -33,14 +33,13 @@
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Melis</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
 <figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
+  <img alt='Melis' src='mIcon.png'>
+  <figcaption>Software and blacksmithing....because they go together....</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+
+
